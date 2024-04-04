85276c1f6367456fdcfc15e8fbc52dbdeaae2e8e
AldyPavet <aldana.pavet.garcia@mi.unc.edu.ar>
2024-04-04 09:45:42 -0300

    step_3_tests

---
+++ src/logic.c
@@ -0,0 +1,20 @@
+#include "logic.h"
+
+int add(int a, int b){
+    return a + b;
+}
+
+int subtract(int a, int b){
+    return a - b;
+}
+
+int multiply(int a, int b){
+    return a * b;
+}
+
+int divide(int a, int b){
+    if(b != 0){
+        return (double)a / b;
+    }
+    return -1.0;
+}
